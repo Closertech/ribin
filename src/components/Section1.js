a36eac25ea05eff3a181d0c2fdec74867a680032
Eshller <20B030016@iitb.ac.in>
2023-06-18 23:46:31 +0530

    changes

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -4,7 +4,7 @@ export default function Section1() {
   return (
     <section className="section1">
       <div className="text-center content-1">
-        <button className="brand-btn">BRANDS THAT TRUST US</button>
+        <button className="brand-btn disabled">BRANDS THAT TRUST US</button>
       </div>
       <img src="./section-1-img.png" width="100%" className="text-center sec-1-img" alt="" />
     </section>
