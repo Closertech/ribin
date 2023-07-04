b3ae41a5efd975036c1c98dab00574828c60ce9c
abhipaiangle <abhishekangle6@gmail.com>
2023-07-05 01:35:00 +0530

    Added Privacy Policy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import Navbar from '../src/components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from '../src/components/Footer';
 import Hero from '../src/components/Hero';
+import Navbar from '../src/components/Navbar';
+import PrivacyPolicy from '../src/components/PrivacyPolicy';
 import Section1 from '../src/components/Section1';
 import Section2 from '../src/components/Section2';
 import Section3 from '../src/components/Section3';
 import Section4 from '../src/components/Section4';
 import Section5 from '../src/components/Section5';
-import Footer from '../src/components/Footer';
-import './App.css'
+import './App.css';
+
 function App() {
   return (
-    <>
-    <Navbar />
-    <Hero />
-    <Section1 />
-    <Section2 />
-    <Section3 />
-    <Section4 />
-    <Section5 />
-    <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/" element={<>
+          <Navbar />
+          <Hero />
+          <Section1 />
+          <Section2 />
+          <Section3 />
+          <Section4 />
+          <Section5 />
+          <Footer />
+        </>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/PrivacyPolicy.js
@@ -0,0 +1,167 @@
+import React from 'react';
+import './PrivacyPolicy.css';
+
+export default class PrivacyPolicy extends React.Component {
+  render() {
+    return (
+      <div class='container'>
+        <h1 class='header'>Privacy Policy</h1>
+        <p>
+        RIBIN (hereinafter referred to as the ‘Portal’) recognizes the importance of maintaining your privacy.<br />
+        The Portal is a service provided by CloserTech Private Limited ("Company"). We value your privacy and appreciate
+        your trust in us. This Policy describes how we treat the user information we collect.<br />
+        The terms ‘we’, ‘our(s)’ or ‘us’ wherever used hereinafter shall refer to Company and the Portal; and the terms
+        ‘you’, ‘yours’ etc. shall refer to the end-users of the Portal who subscribe to it and utilize it to purchase
+        gift cards.<br />
+        This Privacy Policy applies to current and former visitors of our Portal and to our online customers.<br />
+        By visiting and/or using our Portal, you agree to this Privacy Policy.
+        </p>
+        <br />
+        <br />
+        <h2>1. INFORMATION WE COLLECT:</h2>
+        <br />
+        <br />
+        a)<b> Contact information:</b> We might collect your name, email, mobile number, phone number, street, city, state,
+        pin code, country, gender and IP address.
+        <br />
+        <br />
+        b)<b>Payment and billing information:</b> We might collect your billing name, billing address and payment method
+        when you subscribe to the Portal. We do not collect your credit/debit card number, card expiry date, or other
+        details pertaining to your card on our Portal. Payments shall be made through our Portal; however credit/debit card
+        information will be obtained and processed by our online payment partner
+        <br />
+        <br />
+        c) <b>Demographic information:</b> We may collect demographic information about you, products and services that you
+        are interested in, or any other information provided by you during the use of our Portal. We might collect this as a
+        part of a survey also.
+        <br />
+        <br />
+        d)<b> Other information:</b> If you use our Portal, we may collect information about your IP address and the browser
+        you're using. Certain information may also be collected through web crawling.
+        <br />
+        <br />
+        <h2>2. WAYS OF INFORMATION COLLECTION:</h2>
+        <br />
+        <br />
+        a) <b>Directly from you:</b> We collect information directly from you when you subscribe to the Portal. We also
+        collect information if you post a comment on our Portal, or ask us a question through phone or email.
+        <br />
+        <br />
+
+        b)<b> Collected passively from you:</b> We use tracking tools like Google Analytics, Google Webmaster, browser
+        cookies and web beacons for collecting information about your usage of our Portal.
+        <br />
+        <br />
+        c)<b> From third parties:</b> We may collect certain information pertaining to you from third parties. If you use an
+        integrated social media feature on our Portal, the third-party social media site will give us certain information
+        about you. This could include, but may not be limited to, your name and email address.
+        <br />
+        <br />
+        <h2>3. USE OF YOUR PERSONAL INFORMATION:</h2>
+        <br />
+        <br />
+        a)<b> For contacting purposes:</b> We might use the information you provide to contact you for confirmation of a
+        subscription on our Portal or for other promotional purposes.
+        <br />
+        <br />
+        b)<b> Response to your requests or questions:</b> We might use your information to address any queries raised by you
+        regarding the Portal, subscription, or the purchase of any gift cards.
+        <br />
+        <br />
+        c)<b> Improving our services:</b> We might use your information to customize your experience with us. This could
+        include displaying content based upon, and curated as per your preferences.
+        <br />
+        <br />
+        d)<b> Observing site trends and customer interests:</b> We may use your information to make our Portal and services
+        better. We may combine information we get from you with information about you we get from third parties.
+        <br />
+        <br />
+        e)<b> Security purposes: </b>We may use information to protect our company/LLP, our customers, or our Portal.
+        <br />
+        <br />
+        f)<b> Marketing purposes:</b> We might send you information about special promotions or offers on the Portal, or
+        regarding any new features or services. These might be our own offers, or third-party offers that we think will be
+        suitable for you. Furthermore, upon subscription to our Portal, we may enroll you in our newsletter.
+        <br />
+        <br />
+        g)<b> Transactional communications:</b> We might send you emails or SMS about subscriptions to our Portal, the
+        purchase of gift cards through our Portal, or for any other use by you of the Portal and services being offered
+        thereunder which has a transactional element associated with it.
+        <br />
+        <br />
+        We use information as otherwise permitted by law.
+        <br />
+        <br />
+        <br />
+        <h2>4. SHARING OF INFORMATION WITH THIRD-PARTIES:</h2>
+        <br />
+        <br />
+        a)<b> Third parties who perform services on our behalf:</b> We share information with vendors who help us manage our
+        online registration and subscription process, or payment processors, or transactional message processors. Some of
+        these vendors may be located outside of India.
+        <br />
+        <br />
+        b)<b>Business partners:</b> The term ‘Business partners’ includes the third parties whose products/services you can
+        purchase or avail using the gift cards provided by us. Our partners use the information we give them as described in
+        their privacy policies. We will share details, included but not limited to, your name and the amount of the gift
+        card purchased by you.
+        <br />
+        <br />
+        c)<b> Compliance with law:</b> We will share information to respond to a court order or subpoena requiring the same.
+        We may also share it if a government agency or investigatory body requests. Or, we might also share information when
+        we are investigating potential fraud.
+        <br />
+        <br />
+        d)<b>Successor to all or part of our business:</b> If our business is in entirety, or if any part of it thereof is
+        sold, we may give our customer lists and certain personal information about you to our business successor as part of
+        the sale transaction.
+        <br />
+        <br />
+        e) In the event that certain circumstances or situations arise which require us to share your information with
+        third-parties who have not been contemplated in the clause hereinabove, we may share the information if deemed
+        necessary by us. We will inform you regarding the occurrence of any such circumstance or situation prior to sharing
+        your information.
+        <br />
+        <br />
+        <br />
+        <h2>5. THIRD PARTY SITES</h2>
+        <br />
+        <br />
+        If you click on one of the links to third party websites, you may be taken to websites we do not control. This
+        policy does not apply to the privacy practices of those websites. Read the privacy policy of other websites
+        carefully. We are not responsible for these third party sites.
+        <br />
+        <br />
+        <br />
+        <h2>6. GRIEVANCE OFFICER</h2>
+        <br />
+        <br />
+        In accordance with Information Technology Act 2000 and rules made there under, the name and contact details of the
+        Grievance Officer are provided below:
+        <br />
+        Name: Ashwinsingh Chauhan ; Email:<a> ashwin.chauhan@ribin.in</a> ;
+        <br />
+        If you have any questions about this Policy or other privacy concerns, you can also email us at
+        _help@ribin.in________
+        <br />
+        <br />
+        <br />
+
+        <h2>7. UPDATES TO THIS POLICY</h2>
+        <br />
+        <br />
+        This Privacy Policy was last updated in _2023_. From time to time we may change our privacy practices. We will
+        notify you of any material changes to this policy as required by law. We will also post an updated copy on our
+        Portal. Please check our site periodically for updates.
+        <br />
+        <br />
+        <h2>8. JURISDICTION</h2>
+        <br />
+        <br />
+        If you choose to visit and use the Portal, your visit and any dispute over privacy is subject to this Policy and the
+        Portal's terms of use. In addition to the foregoing, any disputes arising under this Policy shall be governed by the
+                    laws of Maharashtra, and shall be subject to the jurisdiction of the courts of Pune, Maharashtra.
+            </div>
+    );
+  }
+}
