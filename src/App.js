55de7de281510082f2c49abeb6d49f14b4397e86
abhipaiangle <abhishekangle6@gmail.com>
2023-09-14 03:29:18 +0530

    Under Construction

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from '../src/components/Footer';
-import Hero from '../src/components/Hero';
-import Navbar from '../src/components/Navbar';
 import PrivacyPolicy from '../src/components/PrivacyPolicy';
-import Section1 from '../src/components/Section1';
-import Section2 from '../src/components/Section2';
-import Section3 from '../src/components/Section3';
-import Section4 from '../src/components/Section4';
-import Section5 from '../src/components/Section5';
 import './App.css';
+import ComingSoon from './components/ComingSoon';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<>
-          <Navbar />
+          {/* <Navbar />
           <Hero />
           <Section1 />
           <Section2 />
           <Section3 />
           <Section4 />
           <Section5 />
-          <Footer />
+          <Footer /> */}
+          <ComingSoon />
         </>} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import * as animationData from '.././coming_soon.json';
+import './ComingSoon.css';
+
+export default function ComingSoon() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+  return (
+      <section className="coming-soon">
+          <img src="./back.png" width="100%" alt="" className='blur'/>
+          <div className='wrapper'>
+              <div className='content'>
+                  <div className='logo-wrapper'>
+                      <img src="./logo-white.png" width={200} alt="" />
+                      <p className='text_uc'>Under Construction</p>
+                  </div>
+              <Lottie 
+                options={defaultOptions}
+                height={260}
+                width={260}
+                  />
+                  <div className="d-flex">
+                    <div className="playstore">
+                      <a href="https://play.google.com/store/apps/details?id=com.closertech.ribin">
+                        <img src="./playstore.png" width="100%" className="playstore-btn" alt="" />
+                      </a>
+                    </div>
+                    <div className="appstore">
+                      <a href="https://apps.apple.com/in/app/ribin-experience-gifting-app/id1667026180">
+                        <img src="./appstore.png" width="100%" className="appstore-btn" alt="" />
+                      </a>
+                    </div>
+                  </div>
+              </div>
+          </div>
+    </section>
+  )
+}
